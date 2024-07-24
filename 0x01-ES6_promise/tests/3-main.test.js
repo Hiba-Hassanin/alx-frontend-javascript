354d4fada0fa4f267ec36d5c26d27c753c11f876
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-07-25 01:54:13 +0200

    returns a Promise

---
+++ 0x01-ES6_promise/tests/3-main.test.js
@@ -0,0 +1,28 @@
+// 3-main.test.js
+
+import handleProfileSignup from './3-all';
+import { uploadPhoto, createUser } from './utils';
+
+jest.mock('./utils');
+
+test('handleProfileSignup logs correct output on success', () => {
+  uploadPhoto.mockResolvedValue({ body: 'photo-profile-1' });
+  createUser.mockResolvedValue({ firstName: 'Guillaume', lastName: 'Salva' });
+
+  const consoleSpy = jest.spyOn(console, 'log');
+  return handleProfileSignup().then(() => {
+    expect(consoleSpy).toHaveBeenCalledWith('photo-profile-1 Guillaume Salva');
+    consoleSpy.mockRestore();
+  });
+});
+
+test('handleProfileSignup logs error message on failure', () => {
+  uploadPhoto.mockRejectedValue(new Error());
+  createUser.mockRejectedValue(new Error());
+
+  const consoleSpy = jest.spyOn(console, 'log');
+  return handleProfileSignup().catch(() => {
+    expect(consoleSpy).toHaveBeenCalledWith('Signup system offline');
+    consoleSpy.mockRestore();
+  });
+});

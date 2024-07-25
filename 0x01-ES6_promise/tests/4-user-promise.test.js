524c548d2ea8c71b8a67e367fc9de396939c4525
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-07-25 02:04:00 +0200

    Returns a resolved promise

--- 0x01-ES6_promise/tests/4-user-promise.test.js
+++ 0x01-ES6_promise/tests/4-user-promise.test.js
@@ -1,10 +1,5 @@
 // 4-user-promise.test.js
 
-import signUpUser from './4-user-promise';
+import signUpUser from "../4-user-promise";
 
-test('signUpUser resolves with correct values', () => {
-  return expect(signUpUser('Bob', 'Dylan')).resolves.toEqual({
-    firstName: 'Bob',
-    lastName: 'Dylan',
-  });
-});
+console.log(signUpUser("Bob", "Dylan"));

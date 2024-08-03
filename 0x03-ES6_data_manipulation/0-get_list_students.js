75ab338ea25a02f6c7dfa91f06847df0b7c936db
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-08-03 03:38:01 +0200

    Students list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,12 +4,12 @@
  * Returns a list of students with their details.
  * @returns {Array} An array of student objects.
  */
-function getListStudents() {
-  return [
+export default function getListStudents() {
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-}
 
-export default getListStudents;
+  return students;
+}

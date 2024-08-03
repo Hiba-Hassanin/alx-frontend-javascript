27da9c4f639f431e0faf110551e7b3a42cf3439b
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-08-03 03:58:49 +0200

    Students list

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+// 9-groceries_list.js
+
+/**
+ * Creates a Map of grocery items and their quantities.
+ * @returns {Map} A Map where the key is the item name and the value is the quantity.
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}

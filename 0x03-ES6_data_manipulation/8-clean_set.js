245eed4a41820704ef241e5d2ecde0df4bd8afb3
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-08-03 04:01:34 +0200

    Students list

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,17 +6,17 @@
  * @param {String} startString - The string to check for at the start of each value.
  * @returns {String} A string of filtered values separated by '-'.
  */
- export default function cleanSet(set, startString) {
-   let text = '';
-   const array = [];
+export default function cleanSet(set, startString) {
+  let text = '';
+  const array = [];
 
-   if (startString && typeof startString === 'string') {
-     for (const element of set) {
-       if (element && element.startsWith(startString)) {
-         array.push(element.slice(startString.length));
-       }
-     }
-     text = array.join('-');
-   }
-   return text;
- }
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
+    }
+    text = array.join('-');
+  }
+  return text;
+}

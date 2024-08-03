1bfb773862d2e18536a1be3969b5bac79f5634e5
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-08-03 03:06:53 +0200

    Teacher interface ex

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,33 @@ interface Teacher {
   [key: string]: any;
 }
 
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+// Output:
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
 interface Directors extends Teacher {
   numberOfReports: number;
 }
@@ -28,3 +55,48 @@ console.log(director1);
 // lastName: "Doe"
 // location: "London"
 // numberOfReports: 17
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
+
+// Define an interface for the constructor of the class
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+// Define an interface for the class
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Implement the StudentClass according to the interfaces
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student = new StudentClass('John', 'Doe');
+console.log(student.displayName()); // Output: John
+console.log(student.workOnHomework()); // Output: Currently working

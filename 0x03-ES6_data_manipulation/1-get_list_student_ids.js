8a5e6a9eb80139b6382aa077acc2e866a6a6027c
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-08-03 03:49:18 +0200

    Students list

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,14 +5,10 @@
  * @param {Array} students - The list of student objects.
  * @returns {Array} An array of student ids or an empty array if input is not an array.
  */
- function getListStudentIds(students) {
-   // Check if the input is an array
-   if (!Array.isArray(students)) {
-     return [];
-   }
+export default function getListStudentIds(lst) {
+  if (Array.isArray(lst)) {
+    return lst.map((obj) => obj.id);
+  }
 
-   // Use map to extract ids from the array of student objects
-   return students.map(student => student.id);
- }
-
- export default getListStudentIds;
+  return [];
+}
